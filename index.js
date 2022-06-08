68ac31253de5be4007b234356045f77de9784c78
himlayyy <alesandralelis@gmail.com>
2022-06-08 16:01:01 +0800

    Added Mongoose disconnect & reconnection function

--- index.js
+++ index.js
@@ -3,10 +3,33 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
+// Initial connection to MongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Listener for disconnection
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB connected");
+});
+// Listener for connection
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected")
+})
+
+// Starts server
 app.listen(8000, () => {
+  // Then connects to MongoDB
+  connect();
   console.log("Server is running");
 });
