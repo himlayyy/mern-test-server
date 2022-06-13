eeba37444c7128914c070edbc28c507f7763b36e
himlayyy <alesandralelis@gmail.com>
2022-06-13 23:29:21 +0800

    Defined api routes for authentication & logging in

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { register, login} from "../controllers/authentication.js";
 
 const authRouter = express.Router();
 
-authRouter.get("/", (req, res) => {
-  res.send("In /auth");
-});
+// New user
+authRouter.post("/register", register);
+
+// Login
+authRouter.post("/login", login);
+
 
 export default authRouter;
