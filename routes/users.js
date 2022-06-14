0c2ba00b134e823c701fe52d363de8c9df0e58f1
himlayyy <alesandralelis@gmail.com>
2022-06-14 16:38:38 +0800

    Added verifyAdmin step before allowing access

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,35 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const usersRouter = express.Router();
 
-// Check authentication
-usersRouter.get("/checkauthentication", verifyToken, (req, res, next) =>{
-  // Returns if you are logged in (there is a cookie)
-  res.send("Hello user, you are logged in");
-})
+// // Check authentication
+// usersRouter.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   // Returns if you are logged in (there is a cookie)
+//   res.send("Hello user, you are logged in");
+// });
+
+// usersRouter.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   // Returns if you are logged in (there is a cookie)
+//   res.send("Hello user, you are logged in and you can delete  your account");
+// });
+
+// usersRouter.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   // Returns if you are logged in (there is a cookie)
+//   res.send("Hello admin, you are logged in and you can delete  all accounts");
+// });
 
 // Get users
-usersRouter.get("/", getUsers);
+usersRouter.get("/", verifyAdmin, getUsers);
 
 // Get user
-usersRouter.get("/:id", getUser);
+usersRouter.get("/:id",  verifyUser, getUser);
 
 // Delete users
-usersRouter.delete("/:id", deleteUser);
+usersRouter.delete("/:id",  verifyAdmin,deleteUser);
 
 // Update users
-usersRouter.put("/", updateUser);
+usersRouter.put("/:id", verifyUser, updateUser);
 export default usersRouter;
