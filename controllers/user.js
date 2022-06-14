8f5d182467710b6b60e1126456419025f3f92a7e
himlayyy <alesandralelis@gmail.com>
2022-06-14 14:09:42 +0800

    Created User controller, routes

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+import User from "../models/users.js";
+import { createError } from "../utils/error.js";
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const getUser = await User.findById(req.params.id);
+
+    res.status(200).json(getUser);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getUsers = async (req, res, next) => {
+  try {
+    const getUsers = await User.find();
+    if (getUser) return res.status(200).json(getUsers);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", (req, res) => {
-  res.send("In /users");
-});
+// Get users
+usersRouter.get("/", getUsers);
 
+// Get user
+usersRouter.get("/:id", getUser);
+
+// Delete users
+usersRouter.delete("/:id", deleteUser);
+
+// Update users
+usersRouter.put("/", updateUser);
 export default usersRouter;
