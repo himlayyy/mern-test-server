278b844ea72264f20631a52cbf07748910c6a1cc
himlayyy <alesandralelis@gmail.com>
2022-06-14 10:03:22 +0800

    Changed json response to just user contents

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -32,7 +32,7 @@ export const login = async (req, res, next) => {
       if (!result) return next(createError(404, "Wrong password or username"));
     });
 
-    res.status(200).json({ message: "logged in!", user });
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
