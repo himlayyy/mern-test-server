faf490c123f2bee3fd1391cc7ae0600c7d33a42f
himlayyy <alesandralelis@gmail.com>
2022-06-14 14:24:51 +0800

    Added checkauthentication function to verify token/logged in

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,15 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const usersRouter = express.Router();
+
+// Check authentication
+usersRouter.get("/checkauthentication", verifyToken, (req, res, next) =>{
+  // Returns if you are logged in (there is a cookie)
+  res.send("Hello user, you are logged in");
+})
 
 // Get users
 usersRouter.get("/", getUsers);
