4b30e48a26783ee9f7bfbac08f62efff5ca57499
himlayyy <alesandralelis@gmail.com>
2022-06-15 16:27:35 +0800

    Manually getting all hotel types instead of req.query

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -73,17 +73,27 @@ export const countByCity = async (req, res, next) => {
 };
 
 export const countByType = async (req, res, next) => {
-  const types = req.query.type.split(",");
-
+  // const types = req.query.type.split(",");
   try {
-    const list = await Promise.all(
-      types.map((type) => {
-        return Hotel.countDocuments({ type: type });
-      })
-    );
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cottageCount = await Hotel.countDocuments({ type: "cottage" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
 
-    res.status(200).json(list);
+
+
+    res.status(200).json([
+      { type: "hotels", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cottages", count: cottageCount },
+      { type: "cabins", count: cabinCount },
+    ]);
   } catch (err) {
     next(err);
   }
 };
+;
