0ecfcc273307150e6b6154e76b29a5741219fe2e
himlayyy <alesandralelis@gmail.com>
2022-06-15 13:46:23 +0800

    Add countByCity and countByType endpoints

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -55,3 +55,35 @@ export const getHotels = async (req, res, next) => {
     next(err);
   }
 };
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const countByType = async (req, res, next) => {
+  const types = req.query.type.split(",");
+
+  try {
+    const list = await Promise.all(
+      types.map((type) => {
+        return Hotel.countDocuments({ type: type });
+      })
+    );
+
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
