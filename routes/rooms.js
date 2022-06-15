92bf1ca43646cc8c4b3cd5dadfca1f3fda25702a
himlayyy <alesandralelis@gmail.com>
2022-06-15 13:03:05 +0800

    Added hotelId

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,23 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getAllRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import Rooms from "../models/rooms.js";
 
 const roomsRouter = express.Router();
 
-roomsRouter.get("/", (req, res) => {
-  res.send("In /rooms");
-});
+// Create room
+roomsRouter.post("/:hotelId", verifyAdmin, createRoom);
+
+// Update room
+roomsRouter.put("/:id", verifyAdmin,updateRoom);
+
+// Delete room
+roomsRouter.delete("/:hotelId/:roomId", verifyAdmin, deleteRoom);
+
+// Get room
+roomsRouter.get("/", getRoom);
+
+// Get rooms
+roomsRouter.get("/", getAllRooms);
 
 export default roomsRouter;
