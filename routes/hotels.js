1831093538f481c7c319942bcf35ca1690599938
himlayyy <alesandralelis@gmail.com>
2022-06-15 13:47:18 +0800

    Add countByCity and countByType routes

--- routes/hotels.js
+++ routes/hotels.js
@@ -6,6 +6,8 @@ import {
   getHotel,
   getHotels,
   updateHotel,
+  countByCity,
+  countByType,
 } from "../controllers/hotels.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 import hotels from "../models/hotels.js";
@@ -13,7 +15,8 @@ import hotels from "../models/hotels.js";
 const hotelsRouter = express.Router();
 
 // Create
-hotelsRouter.post("/:hotelId", verifyAdmin, createHotel);
+// hotelsRouter.post("/:hotelId", verifyAdmin, createHotel);
+hotelsRouter.post("/", verifyAdmin, createHotel);
 
 // Update
 hotelsRouter.put("/:id", verifyAdmin, updateHotel);
@@ -22,10 +25,12 @@ hotelsRouter.put("/:id", verifyAdmin, updateHotel);
 hotelsRouter.delete("/:id", verifyAdmin, deleteHotel);
 
 // Get one
-hotelsRouter.get("/:id", getHotel);
+hotelsRouter.get("/find/:id", getHotel);
 
 //Get all
 hotelsRouter.get("/", getHotels);
 
-
+// Hotel details GET queries
+hotelsRouter.get("/countByCity", countByCity);
+hotelsRouter.get("/countByType", countByType);
 export default hotelsRouter;
