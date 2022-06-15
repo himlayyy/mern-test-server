0d2d062af805ddb9d76c27ebde23c426c6830769
himlayyy <alesandralelis@gmail.com>
2022-06-15 13:02:39 +0800

    Added verifyAdmin check

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,22 +1,28 @@
 import express from "express";
 import Hotel from "../models/hotels.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotels.js";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotels.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const hotelsRouter = express.Router();
 
 // Create
-hotelsRouter.post("/", verifyAdmin, createHotel);
+hotelsRouter.post("/:hotelId", verifyAdmin, createHotel);
 
 // Update
-hotelsRouter.put("/:id", verifyAdmin ,updateHotel);
+hotelsRouter.put("/:id", verifyAdmin, updateHotel);
 
 // Delete
-hotelsRouter.delete("/:id",verifyAdmin, deleteHotel);
+hotelsRouter.delete("/:id", verifyAdmin, deleteHotel);
 
 // Get one
 hotelsRouter.get("/:id", getHotel);
 
 //Get all
-hotelsRouter.get("/",getHotels );
+hotelsRouter.get("/", getHotels);
 export default hotelsRouter;
